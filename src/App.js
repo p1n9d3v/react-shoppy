c5c952a28fe6edbe29cbf13a9fcb5d95cd52ed35
KIM BEOMJOON <p1n9@KIMs-MacBook-Air.local>
2024-01-24 16:31:03 +0900

    style: reset css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from "react";
+import Home from 'pages/Home';
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './style/index.css';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Home />,
+        errorElement: <div>Something wrong</div>
+    }
+]);
 
 function App() {
-    return <div className="App"></div>;
+    return <RouterProvider router={router} />;
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
+const px0_1000 = { ...Array.from(Array(1000)).map((_, i) => `${i}px`) };
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: [],
+    theme: {
+        extend: {
+            fontSize: px0_1000,
+            width: px0_1000,
+            height: px0_1000,
+            maxWidth: px0_1000,
+            maxHeight: px0_1000,
+            minWidth: px0_1000,
+            minHeight: px0_1000,
+            spacing: px0_1000,
+            borderWidth: px0_1000
+        }
+    },
+    plugins: []
+};
