c40e43eea2556a025a3463f45e376cf2e1a935fb
KIM BEOMJOON <p1n9@KIMs-MacBook-Air.local>
2024-04-03 17:45:47 +0900

    feat(UserAvatar): Add UserAvatar component

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,3 +1,4 @@
+import UserAvatar from 'components/ui/UserAvatar';
 import React from 'react';
 import { CgShoppingCart } from 'react-icons/cg';
 import { SiShopify } from 'react-icons/si';
@@ -10,20 +11,19 @@ function Header() {
     const { user, login, logout } = useUser();
     const navigate = useNavigate();
 
-    const goHome = () => navigate('/');
-
     return (
         <div className={cn('flex justify-between items-center py-12 px-24')}>
             <div
                 className={cn('flex items-center gap-8 text-28 cursor-pointer')}
-                onClick={goHome}
+                onClick={() => navigate('/')}
             >
                 <SiShopify size="36" className={cn('text-primary')} />
                 Shoppy
             </div>
-            <div className={cn('flex items-center gap-8')}>
+            <div className={cn('flex items-center gap-12')}>
                 <div>products</div>
                 <CgShoppingCart size="32" />
+                {user && <UserAvatar user={user} />}
                 <Button
                     size="lg"
                     tw="uppercase"
